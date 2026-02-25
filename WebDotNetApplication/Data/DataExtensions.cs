d388d301a55b0bde6121e254d2c0cfefa9152762
DZT711 <nguyensihuynsh711@gmail.com>
2026-02-25 15:05:16 +0700

    moved logic block ouside the main cs file

--- WebDotNetApplication/Data/DataExtensions.cs
+++ WebDotNetApplication/Data/DataExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using WebDotNetApplication.Models;
 
 namespace WebDotNetApplication.Data;
 
@@ -10,5 +11,38 @@ public static class DataExtensions
         using var scope = app.Services.CreateScope();
         var context = scope.ServiceProvider.GetRequiredService<WebAppContext>();
         context.Database.Migrate();
+    }
+    public static void AddDataToDatabase(this WebApplicationBuilder builder)
+    {
+        var conn = "Data Source=App.db";
+        builder.Services.AddSqlite<WebAppContext>(
+                        conn,
+                        optionsAction: options => options.UseSeeding((context, _) =>
+                        {
+                            if (!context.Set<Role>().Any())
+                            {
+                                context.Set<Role>().AddRange(
+                                    new Role { NameRole = "Admin" },
+                                    new Role { NameRole = "Sigma" },
+                                    new Role { NameRole = "Chad" },
+                                    new Role { NameRole = "Chillman" },
+                                    new Role { NameRole = "Manager" },
+                                    new Role { NameRole = "Supervisor" },
+                                    new Role { NameRole = "Dominance" },
+                                    new Role { NameRole = "Salvage" },
+                                    new Role { NameRole = "King" },
+                                    new Role { NameRole = "Queen" },
+                                    new Role { NameRole = "Lord" },
+                                    new Role { NameRole = "Daddy" },
+                                    new Role { NameRole = "Overlord" },
+                                    new Role { NameRole = "Shogun" },
+                                    new Role { NameRole = "Pharaoh" },
+                                    new Role { NameRole = "User" }
+                                );
+                                context.SaveChanges();
+                            }
+                        }
+                    )
+                );
     }
 }
--- WebDotNetApplication/Program.cs
+++ WebDotNetApplication/Program.cs
@@ -5,42 +5,9 @@ using WebDotNetApplication.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddValidation();
-var conn = "Data Source=App.db";
-builder.Services.AddSqlite<WebAppContext>(conn);
+builder.AddDataToDatabase();
 var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<WebAppContext>();
-    context.Database.EnsureCreated();
-
-    if (!context.Set<Role>().Any())
-    {
-        context.Set<Role>().AddRange(
-            new Role { NameRole = "Admin" },
-            new Role { NameRole = "Sigma" },
-            new Role { NameRole = "Chad" },
-            new Role { NameRole = "Chillman" },
-            new Role { NameRole = "Manager" },
-            new Role { NameRole = "Supervisor" },
-            new Role { NameRole = "Dominance" },
-            new Role { NameRole = "Salvage" },
-            new Role { NameRole = "King" },
-            new Role { NameRole = "Queen" },
-            new Role { NameRole = "Lord" },
-            new Role { NameRole = "Daddy" },
-            new Role { NameRole = "Overlord" },
-            new Role { NameRole = "Shogun" },
-            new Role { NameRole = "Pharaoh" },
-            new Role { NameRole = "User" }
-        );
-        context.SaveChanges();
-    }
-}
-
 app.MapGet("/", () => "Hello World!");
-
-
 app.MapUserEndpoints();
 app.MigrateDb(); // auto create database when not exist
 app.Run();
